659ca02cedb67998544d2d5e8228694bccca31e4
pcwii <pmchenier@gmail.com>
2018-07-19 19:24:10 -0400

    Added Movie Information Popup

--- __init__.py
+++ __init__.py
@@ -194,12 +194,12 @@ class KodiSkill(MycroftSkill):
         # time.sleep(1)  # add delay to avoid socket timeout
         kodi_id.Playlist.Add(playlistid=1, item={'movieid': movieid})
         # time.sleep(1)  # add delay to avoid socket timeout
-        self.kodi_payload = '{"jsonrpc":"2.0","method":"player.open", "params": {"item":{"playlistid":1}}}'
+        self.kodi_payload = '{"jsonrpc":"2.0","method":"player.open", "params": {"item":{"playlistid":1}},}'
         try:
             self.json_response = requests.post(self.kodi_path, data=self.kodi_payload, headers=self.json_header)  # start directly with json request
         except:
             time.sleep(0.5)  # ignore timeout error
-            self.speak("jason error")
+            # self.speak("jason error")
 
     @adds_context('Navigate')
     def play_film_by_search(self, kodi_id, film_search):  # called from, handle_play_film_intent
@@ -269,7 +269,7 @@ class KodiSkill(MycroftSkill):
                                       headers=self.json_header)  # start directly with json request
         except:
             time.sleep(0.5)  # ignore timeout error
-            self.speak("jason error")
+            # self.speak("jason error")
 
 
     def stop(self):
