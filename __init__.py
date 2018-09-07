4536aacc979e78b64ba7f920fb4a14b351153309
pcwii <pmchenier@gmail.com>
2018-09-07 17:00:38 -0400

    Began adding support for username and passwords Began migrating all functions to JSON requests

--- __init__.py
+++ __init__.py
@@ -91,8 +91,7 @@ class KodiSkill(MycroftSkill):
                     kodi_user = self.settings["kodi_user"]
                     kodi_pass = self.settings["kodi_pass"]
                     self.kodi_instance = Kodi(kodi_ip)
-                    self.kodi_path = "http://"+kodi_ip+":"+kodi_port+"/jsonrpc"
-                    # self.kodi_payload = '{"jsonrpc":"2.0","method":"player.open", "params": {"item":{"playlistid":1}}}'
+                    self.kodi_path = "http://"+kodi_user+":"+kodi_pass+"@"+kodi_ip+":"+kodi_port+"/jsonrpc"
                     self._is_setup = True
             except Exception as e:
                 LOG.error(e)
@@ -211,7 +210,7 @@ class KodiSkill(MycroftSkill):
         # time.sleep(1)  # add delay to avoid socket timeout
         kodi_id.Playlist.Add(playlistid=1, item={'movieid': movieid})
         # time.sleep(1)  # add delay to avoid socket timeout
-        self.kodi_payload = '{"jsonrpc":"2.0","method":"player.open", "params": {"item":{"playlistid":1}}}'
+        self.kodi_payload = '{"jsonrpc": "2.0", "method": "player.open", "params": {"item":{"playlistid":1}}, "id": 1}'
         try:
             self.json_response = requests.post(self.kodi_path, data=self.kodi_payload, headers=self.json_header)  # start directly with json request
         except Exception as e:
@@ -279,7 +278,7 @@ class KodiSkill(MycroftSkill):
                     optionally('KodiKeyword').optionally('FilmKeyword').
                     build())
     def handle_show_movie_info_intent(self, message):
-        self.kodi_payload = '{"jsonrpc":"2.0","method":"Input.Info", "params": {}}}'
+        self.kodi_payload = '{"jsonrpc":"2.0","method":"Input.Info", "params": {}}, "id": 1}'
         try:
             self.json_response = requests.post(self.kodi_path, data=self.kodi_payload,
                                                headers=self.json_header)  # start directly with json request
